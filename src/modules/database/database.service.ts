00e403e9f123541eaf9d9db3b14c2b05b981f1b8
teolin <teolin@gmail.com>
2024-06-16 09:40:29 +0300

    fix: db

--- src/modules/database/database.service.ts
+++ src/modules/database/database.service.ts
@@ -1,25 +1,24 @@
-import { Database } from '../interfaces';
-
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs'
+import path from 'path'
+import { Database } from '../interfaces'
 
 export default class DatabaseService {
-  private static db: Database;
-  private static filePath = path.join(__dirname, './db.json');
+  private static db: Database
+  private static filePath = path.join(__dirname, './db.json')
 
   static init(): void {
-    this.db = JSON.parse(fs.readFileSync(this.filePath, 'utf8'));
+    this.db = JSON.parse(fs.readFileSync(this.filePath, 'utf8'))
   }
 
   static getData(): Database {
-    return this.db;
+    return this.db
   }
 
   static setData(data: Database): void {
-    this.db = data;
+    this.db = data
   }
 
   static saveToDisk(): void {
-    fs.writeFileSync(this.filePath, JSON.stringify(this.db), 'utf8');
+    fs.writeFileSync(this.filePath, JSON.stringify(this.db), 'utf8')
   }
 }
