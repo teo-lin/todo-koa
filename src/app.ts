919c2b4171a1f500d68bfc6adead3da9d95a303d
teolin <teolin@gmail.com>
2024-06-16 14:48:01 +0300

    feat: start time

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,34 @@
-import Koa from 'koa';
-import bodyParser from 'koa-bodyparser';
-import userRouter from './modules/user/user.controller';
-import taskRouter from './modules/task/task.controller';
-import listRouter from './modules/list/list.controller';
-import DatabaseService from './modules/database/database.service';
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import userRouter from './modules/user/user.controller'
+import taskRouter from './modules/task/task.controller'
+import listRouter from './modules/list/list.controller'
+import DatabaseService from './modules/database/database.service'
+
+const startTime = process.hrtime()
 
 // DATABASE
-DatabaseService.init();
+DatabaseService.init()
 
 // ROUTER
-const app: Koa = new Koa();
+const app: Koa = new Koa()
 
 // MIDDLEWARE
-app.use(bodyParser());
+app.use(bodyParser())
 
 // ROUTES
-app.use(userRouter.routes());
-app.use(taskRouter.routes());
-app.use(listRouter.routes());
+app.use(userRouter.routes())
+app.use(taskRouter.routes())
+app.use(listRouter.routes())
 app.use(async (ctx, next) => {
-  if (ctx.path === '/api') ctx.body = 'Hello World!';
-  else await next();
-});
+  if (ctx.path === '/api') ctx.body = 'Hello World!'
+  else await next()
+})
 
 // SERVER
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+const PORT = 3000
+const endTime = process.hrtime(startTime)
+const startupTime = (endTime[0] * 1000 + endTime[1] / 1000000).toFixed(3)
+app.listen(PORT, () =>
+  console.log(`Server started in ${startupTime} ms on http://localhost:${PORT}`)
+)
